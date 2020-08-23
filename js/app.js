2f56b29f860d30a8c1704d10218d2081b798e4b1
Mallory Eastburn <mallorykaitlin@gmail.com>
2020-08-23 17:43:24 -0500

    Updated JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-
+document
+  .querySelector("#message-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let message = document.querySelector("#message").value;
+    if (message) {
+      document.querySelector(".message-content").innerHTML = message;
+      document.querySelector("#message").value = null;
+    } else {
+      document.querySelector(".feedback").classList.add("show");
+      setTimeout(function () {
+        document.querySelector(".feedback").classList.remove("show");
+      }, 1000);
+    }
+  });
